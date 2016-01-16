2cadb5e2ef8343093d6e020d6d3f293fb81ddea9
bartoszbinda <janisze@outlook.com>
2016-01-16 18:30:05 +0000

    Trying to make meals CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :products
-  resources :items
+  resources :meals
+  post '/products/new', to: "products#new"
   root "pages#home"
   get '/home', to: "pages#home"
   
