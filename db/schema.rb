c0fd57749778162847c628ceeae39d6603deddd9
bartoszbinda <janisze@outlook.com>
2016-01-16 17:17:29 +0000

    Fixed error with scale in products (4 on scale instead 2)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160116170638) do
+ActiveRecord::Schema.define(version: 20160116171030) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20160116170638) do
   create_table "items", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.decimal  "price",       precision: 19, scale: 2
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
   end
 
   create_table "recipes", force: :cascade do |t|
