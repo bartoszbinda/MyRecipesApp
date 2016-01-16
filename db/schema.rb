e4ac6bc6ddb079bac21f50511894aac683278ec9
bartoszbinda <janisze@outlook.com>
2016-01-16 19:37:35 +0000

    create CR in meal controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160116171030) do
+ActiveRecord::Schema.define(version: 20160116184227) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20160116171030) do
   create_table "items", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "meals", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.decimal  "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "products", force: :cascade do |t|
